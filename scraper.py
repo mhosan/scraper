b648c27f460f1e848a50d16e1d727a1941c7ddc7
marcelo <mhosan@gmail.com>
2021-09-23 19:28:42 -0300

    parseando la noticia

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import requests
 import lxml.html as html
 import sys
+import os #para crear carpetas
+import datetime  #manejo de fechas
 
 original_stdout = sys.stdout
 
@@ -13,19 +15,36 @@ XPATH_TITLE = '//h2[@data-h="45"]/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p/text()'
 
+def parse_notice():
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
 #def parse_home():
 try:
     response = requests.get(HOME_URL)
     if response.status_code == 200:
         home = response.content.decode('utf-8')
-        with open ('prueba.txt', 'w') as f:
-            sys.stdout = f
-            print(home)
-            sys.stdout = original_stdout
+        #with open ('prueba.txt', 'w') as f:
+        #    sys.stdout = f
+        #    print(home)
+        #    sys.stdout = original_stdout
         parsed = html.fromstring(home)
         links_to_notices = parsed.xpath('//text-fill')
-        print (links_to_notices)
-        print (len(links_to_notices))
+        #print (links_to_notices)
+        #print (len(links_to_notices))
+        today = datetime.date.today().strftime('%d-%m-%Y')
+        if not os.path.isdir(today):
+            os.mkdir(today)
+        for link in links_to_notices:
+            parse_notice(link, today)
     else:
         raise ValueError(f'Error: {response.status_code}')
 except ValueError as ve:
