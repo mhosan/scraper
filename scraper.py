36f5858293ec4e3d05f5e5c04c8b6015ada54718
marcelo <mhosan@gmail.com>
2021-09-23 19:15:55 -0300

    guardado datos iniciales en un archivo para detectar que aparece cambiado el h2 por text-fill

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import requests
 import lxml.html as html
-import lxml.etree as etree
+import sys
+
+original_stdout = sys.stdout
 
 HOME_URL = 'https://www.larepublica.co/'
 HOME_URL2 = 'https://www.clarin.com/'
 #XPATH_LINK_TO_ARTICLE = '//h2[@data-h="17"]/a/@href'
-XPATH_LINK_TO_ARTICLE = '//h2[@data-h="17"]/a/@href'
+XPATH_LINK_TO_ARTICLE = '//text-fill[not(@class)]/a/@href'
 XPATH_LINK_TO_ARTICLE2 = '//article[@class="content-nota list-format twoxone_no_foto"]/a[@class="link_article"]/@href'
 XPATH_TITLE = '//h2[@data-h="45"]/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
@@ -15,11 +17,15 @@ XPATH_BODY = '//div[@class="html-content"]/p/text()'
 try:
     response = requests.get(HOME_URL)
     if response.status_code == 200:
-        #home = response.content.decode('utf-8')
-        home = response.content
+        home = response.content.decode('utf-8')
+        with open ('prueba.txt', 'w') as f:
+            sys.stdout = f
+            print(home)
+            sys.stdout = original_stdout
         parsed = html.fromstring(home)
-        links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
+        links_to_notices = parsed.xpath('//text-fill')
         print (links_to_notices)
+        print (len(links_to_notices))
     else:
         raise ValueError(f'Error: {response.status_code}')
 except ValueError as ve:
