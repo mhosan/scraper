1460880345442a1763b679d5cac6c39216029202
marcelo <mhosan@gmail.com>
2021-09-24 19:03:13 -0300

    funca leyendo todo

--- scraper.py
+++ scraper.py
@@ -8,19 +8,38 @@ original_stdout = sys.stdout
 
 HOME_URL = 'https://www.larepublica.co/'
 HOME_URL2 = 'https://www.clarin.com/'
-#XPATH_LINK_TO_ARTICLE = '//h2[@data-h="17"]/a/@href'
 XPATH_LINK_TO_ARTICLE = '//text-fill[not(@class)]/a/@href'
 XPATH_LINK_TO_ARTICLE2 = '//article[@class="content-nota list-format twoxone_no_foto"]/a[@class="link_article"]/@href'
-XPATH_TITLE = '//h2[@data-h="45"]/span/text()'
+XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p/text()'
 
-def parse_notice():
+def parse_notice(link, today):
     try:
         response = requests.get(link)
         if response.status_code == 200:
             notice = response.content.decode('utf-8')
+            notice = notice.replace('\"', '')
+            #with open ('noticias.txt', 'w') as file:
+            #    sys.stdout = file
+            #    print(notice)
+            #    sys.stdout = original_stdout
             parsed = html.fromstring(notice)
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"', '')
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
@@ -37,9 +56,10 @@ try:
         #    print(home)
         #    sys.stdout = original_stdout
         parsed = html.fromstring(home)
-        links_to_notices = parsed.xpath('//text-fill')
+        links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
         #print (links_to_notices)
-        #print (len(links_to_notices))
+        #print (f'La lista tiene {len(links_to_notices)} elementos. Elemento 0: {links_to_notices[0]}')
+        
         today = datetime.date.today().strftime('%d-%m-%Y')
         if not os.path.isdir(today):
             os.mkdir(today)
