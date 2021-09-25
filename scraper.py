7fd6a7451bda5aecb08e53b476f9794f0ff72d01
marcelo <mhosan@gmail.com>
2021-09-25 19:47:59 -0300

    armado de la versión para el gran diario argentino

--- scraper.py
+++ scraper.py
@@ -7,29 +7,50 @@ import datetime  #manejo de fechas
 original_stdout = sys.stdout
 
 HOME_URL = 'https://www.clarin.com/'
+
+#Links de los titulares principales y secundarios (los secundarios con y sin foto)
 XPATH_LINK_TO_ARTICLE_MAIN = '//article[@class="content-nota list-format twoxone_no_foto"]/a[@class="link_article"]/@href'
 XPATH_LINK_TO_ARTICLE_SECONDARY = '//article[@class="content-nota list-format onexone_no_foto no-p"]/a[@class="link_article"]/@href'
 XPATH_LINK_TO_ARTICLE_SECONDARY_FOTO = '//article[@class="content-nota onexone_foto    list-format flex-change"]/a[@class="link_article"]/@href'
-XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/span/text()'
-XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
-XPATH_BODY = '//div[@class="html-content"]/p/text()'
+
+#titulos de las notas del los titulares
+XPATH_TITLE_MAIN = '//div[@class="title"]/h1[@id="title"]/text()'
+
+#resumen de las notas principales o titulares
+XPATH_SUMMARY_MAIN = '//div[@class="bajada"]/h2/text()'
+
+#cuerpo de la noticia principal
+XPATH_BODY_MAIN = '//div[@class="body-nota"]/p/text()'
+
 
 def parse_notice(link, today):
     try:
+        #print ('\n\n')
+        #print(f'El link del titular de la nota es: {link}')
         response = requests.get(link)
         if response.status_code == 200:
             notice = response.content.decode('utf-8')
             notice = notice.replace('\"', '')
-            #with open ('noticias.txt', 'w') as file:
-            #    sys.stdout = file
-            #    print(notice)
-            #    sys.stdout = original_stdout
+            """
+            with open ('noticiasPpales.txt', 'w') as file:
+                sys.stdout = file
+                print(notice)
+                sys.stdout = original_stdout
+            """
             parsed = html.fromstring(notice)
             try:
-                title = parsed.xpath(XPATH_TITLE)[0]
+                title = parsed.xpath(XPATH_TITLE_MAIN)[0]
                 title = title.replace('\"', '')
-                summary = parsed.xpath(XPATH_SUMMARY)[0]
-                body = parsed.xpath(XPATH_BODY)
+                #print ('\n\n')
+                #print(f'El titulo es: {title}')
+                summary = parsed.xpath(XPATH_SUMMARY_MAIN)[0]
+                #print ('\n\n')
+                #print(f'El resumen es: {summary}')
+                body = parsed.xpath(XPATH_BODY_MAIN)
+                #for item in body:
+                #    item = item.replace('\'', '')
+                print ('\n\n')
+                print(f'El cuerpo de la nota es: {body}')
             except IndexError:
                 return
             with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
@@ -40,10 +61,11 @@ def parse_notice(link, today):
                 for p in body:
                     f.write(p)
                     f.write('\n')
+            
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
-        print(ve)
+        print(f'El error: {ve}')
 
 
 try:
@@ -55,15 +77,18 @@ try:
         #    print(home)
         #    sys.stdout = original_stdout
         parsed = html.fromstring(home)
-        links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE_SECONDARY_FOTO)
+        links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE_MAIN)
+        print ('\n')
         print (links_to_notices)
-        print (f'La lista tiene {len(links_to_notices)} elementos. Elemento 0: {links_to_notices[0]}')
-        
+        print ('\n')
+        print (f'La lista tiene {len(links_to_notices)} elementos.')
         today = datetime.date.today().strftime('%d-%m-%Y')
-        #if not os.path.isdir(today):
-        #    os.mkdir(today)
-        #for link in links_to_notices:
-        #    parse_notice(link, today)
+        if not os.path.isdir(today):
+            os.mkdir(today)
+        for link in links_to_notices:
+            link = 'https://www.clarin.com' + link 
+            parse_notice(link, today)
+            
     else:
         raise ValueError(f'Error: {response.status_code}')
 except ValueError as ve:
