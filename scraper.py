1001240a777344f54e0f3820365624a8ac0bc3c5
marcelo <mhosan@gmail.com>
2021-09-26 17:27:16 -0300

    lecheando

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ original_stdout = sys.stdout
 HOME_URL = 'https://www.vea.com.ar/leche?map=ft'
 XPATH_HOW_MANY_PRODUCTS = '//div[@class="vtex-search-result-3-x-totalProducts--layout pv5 ph9 bn-ns bt-s b--muted-5 tc-s tl t-action--small"]/span/text()'
 XPATH_PRODUCTS_LIST = '//section[@class="vtex-product-summary-2-x-container vtex-product-summary-2-x-containerNormal overflow-hidden br3 h-100 w-100 flex flex-column justify-between center tc" and @style="max-width:300px"]//a/@href'
-XPATH_PRODUCT_DESCRIPTION = '//h1[@class="vtex-store-components-3-x-productNameContainer mv0 t-heading-4"]/span/text()'
+XPATH_PRODUCT_DESCRIPTION = '//h1[@class="vtex-store-components-3-x-productNameContainer mv0 t-heading-4"]/span[@class="vtex-store-components-3-x-productBrand "]/text()'
 XPATH_PRODUCT_PRICE_INTEGER = '//span[@class="vtex-product-price-1-x-currencyContainer vtex-product-price-1-x-currencyContainer--shelf-main-selling-price"]/span[@class="vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price"]/text()'
 XPATH_PRODUCT_PRICE_DECIMAL = '//span[@class="vtex-product-price-1-x-currencyContainer vtex-product-price-1-x-currencyContainer--shelf-main-selling-price"]/span[@class="vtex-product-price-1-x-currencyFraction vtex-product-price-1-x-currencyFraction--shelf-main-selling-price"]/text()'
 
@@ -29,48 +29,37 @@ XPATH_PRODUCT_PRICE_DECIMAL = '//span[@class="vtex-product-price-1-x-currencyCon
 #XPATH_BODY_MAIN = '//div[@class="body-nota"]/p/text()'
 
 
-def parse_products(link, today):
+def parse_products(link, today, contador):
     try:
         #print ('\n\n')
         #print(f'El link es: {link}')
         response = requests.get(link)
         if response.status_code == 200:
             producto = response.content.decode('utf-8')
-            producto = producto.replace('\"', '')
+            #producto = producto.replace('\"', '')
             """
             with open ('noticiasPpales.txt', 'w') as file:
                 sys.stdout = file
                 print(notice)
                 sys.stdout = original_stdout
             """
-            #with open(f'paginaVeaLechesListado.txt', 'w', encoding='utf-8') as f:
+            #with open(f'VeaLechesDetalle.txt', 'w', encoding='utf-8') as f:
             #    f.write(producto)
             #    f.write('\n\n')
             parsed = html.fromstring(producto)
             try:
-                title = parsed.xpath(XPATH_TITLE_MAIN)[0]
-                title = title.replace('\"', '')
-                #print ('\n\n')
-                #print(f'El titulo es: {title}')
-                summary = parsed.xpath(XPATH_SUMMARY_MAIN)[0]
-                #print ('\n\n')
-                #print(f'El resumen es: {summary}')
-                body = parsed.xpath(XPATH_BODY_MAIN)
-                #for item in body:
-                #    item = item.replace('\'', '')
+                descripcion = parsed.xpath(XPATH_PRODUCT_DESCRIPTION)[0]
+                #descripcion = descripcion.replace('\"', '')
                 print ('\n\n')
-                print(f'El cuerpo de la nota es: {body}')
-            except IndexError:
-                return
-            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
-                f.write(title)
-                f.write('\n\n')
-                f.write(summary)
-                f.write('\n\n')
-                for p in body:
-                    f.write(p)
-                    f.write('\n')
-            
+                print(f'La descripción del producto {contador} es: {descripcion}')
+                precioEntero = parsed.xpath(XPATH_PRODUCT_PRICE_INTEGER)[0]
+                print(f'El precio (parte entera) es: {precioEntero}')
+                precioDecimal = parsed.xpath(XPATH_PRODUCT_PRICE_DECIMAL)[0]
+                print(f'El precio (parte decimal) es: {precioDecimal}')
+            except IndexError as ie:
+                print(f'El error: {ie}')
+            #with open(f'{today}/Vea.txt', 'w', encoding='utf-8') as f:
+            #    f.write(f'{descripcion}: ${precioEntero},{precioDecimal}')
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
@@ -101,10 +90,11 @@ try:
         today = datetime.date.today().strftime('%d-%m-%Y')
         if not os.path.isdir(today):
             os.mkdir(today)
+        contador = 0
         for link in listadoProductos:
             link = 'https://www.vea.com.ar' + link 
-            parse_products(link, today)
-            break
+            parse_products(link, today, contador)
+            contador = contador + 1
     else:
         raise ValueError(f'Error: {response.status_code}')
 except ValueError as ve:
