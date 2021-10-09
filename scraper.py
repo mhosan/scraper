399b73588a00e4cb4beedd58dfef23993f647eb6
marcelo <mhosan@gmail.com>
2021-10-09 13:28:26 -0300

    dia, vea y disco

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 import sys
 import scraperDisco
 import scraperVea
+import scraperDia
 
 original_stdout = sys.stdout
 
 try:
     scraperDisco.procesarLista()
     scraperVea.procesarLista()
+    scraperDia.procesarLista()
 except ValueError as ve:
     print(ve)
 
---
+++ scraperDia.py
@@ -0,0 +1,31 @@
+import parseProductos
+import parseProducto
+
+"""
+listaUrlsProductos=[
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=leche',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=yerba',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=azucar',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=galletitas',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=gaseosas',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=arroz',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=shampoo',
+'https://diaonline.supermercadosdia.com.ar/busca/?ft=pollo'
+]
+"""
+def procesarLista():
+    listaUrlsProductos=['https://diaonline.supermercadosdia.com.ar/busca/?ft=leche']
+    contador = 1
+    for urlProducto in listaUrlsProductos:
+        try:
+            listadoProductos = parseProductos.parseProductos(urlProducto, "Dia")
+            #print('que corno me devuelve la sub:')
+            #print(listadoProductos)
+            if type(listadoProductos is list and len(listadoProductos) > 0) :
+                for link in listadoProductos:
+                    parseProducto.parseUnProducto(link, contador, "Dia")
+                    contador = contador + 1
+            else:
+                raise ValueError(f'Error en la lista de productdos a parsear')
+        except ValueError as ve:
+            print(ve)
