7ac1089af9bb4a89924566d257c7797bad2c9712
marcelo <mhosan@gmail.com>
2021-10-29 10:50:02 -0300

    nada

--- scraperVea.py
+++ scraperVea.py
@@ -3,9 +3,9 @@ import parseProducto
 
 
 listaUrlsProductos = [
-#'https://www.vea.com.ar/leche?map=ft',
+'https://www.vea.com.ar/leche?map=ft',
 'https://www.vea.com.ar/yerba?map=ft', 
-#'https://www.vea.com.ar/azucar?map=ft',
+'https://www.vea.com.ar/azucar?map=ft',
 'https://www.vea.com.ar/galletitas?map=ft',
 'https://www.vea.com.ar/gaseosas?map=ft',
 'https://www.vea.com.ar/arroz?map=ft',
